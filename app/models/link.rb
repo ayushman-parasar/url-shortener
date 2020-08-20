4e120af2edf8d9eac28336634c5ea5eb1328f883
ayushman-parasar <learnertsk@gmail.com>
2020-08-21 02:14:49 +0530

    validations done

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,15 +1,20 @@
 class Link < ApplicationRecord
   validates_presence_of :shorten_url, :original_url
   validates_uniqueness_of :shorten_url
+  validates :original_url, format: { with: URI::regexp(%w[http https]), message: "Please enter valid URL"}
 
   def generate_short_link
-    self.shorten_url = fresh_short_code
+    self.shorten_url = short_link
   end
 
-  def fresh_short_code
+  def short_link
     loop do 
-      shorten = SecureRandom.uuid[0..6]
-      break shorten unless Link.exists?(shorten_url: shorten) 
+      shorten = get_fresh_code
+      break shorten unless Link.exists?(shorten_url: get_fresh_code)  
     end
+  end
+
+  def get_fresh_code
+    SecureRandom.uuid[0..6]
   end
 end
--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -12,5 +12,15 @@ class LinkTest < ActiveSupport::TestCase
     link2 = Link.new(original_url: "https://www.google.com", shorten_url: @link.shorten_url)
     assert_not link2.valid?
   end 
+
+  test "original url must not be empty" do
+    @link.original_url = ""
+    assert_not @link.valid?
+  end
+
+  test "original url should be in proper format" do
+    @link.original_url = "ddsadsadasd"
+    assert_not @link.valid?
+  end
  
 end
